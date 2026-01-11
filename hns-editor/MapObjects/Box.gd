88336221c9bf7560d60e9dcf374c26f61f06cc41
Gee <alickoamarko@gmail.com>
2026-01-11 05:21:58 +0100

    Removed spaces from MapObject data fields cause network serializer I wrote doesn't support them (unnecessary)

--- hns-editor/MapObjects/Box.gd
+++ hns-editor/MapObjects/Box.gd
@@ -5,28 +5,28 @@ func _init() -> void:
 	mesh = BoxMesh.new()
 	
 	data = {
-		"Color R": 255.0,
-		"Color G": 255.0,
-		"Color B": 255.0
+		"Color_R": 255.0,
+		"Color_G": 255.0,
+		"Color_B": 255.0
 	}
 
 @onready var material := StandardMaterial3D.new()
 func _physics_process(_delta: float) -> void:
 	if position.y < 0.0: position.y = 0.0
-		
-	data["Color R"] = roundf(data["Color R"])
-	if data["Color R"] < 0.0: data["Color R"] = 0.0
-	elif data["Color R"] > 255.0: data["Color R"] = 255.0
-	material.albedo_color.r8 = int(data["Color R"])
 	
-	data["Color G"] = roundf(data["Color G"])
-	if data["Color G"] < 0.0: data["Color G"] = 0.0
-	elif data["Color G"] > 255.0: data["Color G"] = 255.0
-	material.albedo_color.g8 = int(data["Color G"])
+	data["Color_R"] = roundf(data["Color_R"])
+	if data["Color_R"] < 0.0: data["Color_R"] = 0.0
+	elif data["Color_R"] > 255.0: data["Color_R"] = 255.0
+	material.albedo_color.r8 = int(data["Color_R"])
 	
-	data["Color B"] = roundf(data["Color B"])
-	if data["Color B"] < 0.0: data["Color B"] = 0.0
-	elif data["Color B"] > 255.0: data["Color B"] = 255.0
-	material.albedo_color.b8 = int(data["Color B"])
+	data["Color_G"] = roundf(data["Color_G"])
+	if data["Color_G"] < 0.0: data["Color_G"] = 0.0
+	elif data["Color_G"] > 255.0: data["Color_G"] = 255.0
+	material.albedo_color.g8 = int(data["Color_G"])
+	
+	data["Color_B"] = roundf(data["Color_B"])
+	if data["Color_B"] < 0.0: data["Color_B"] = 0.0
+	elif data["Color_B"] > 255.0: data["Color_B"] = 255.0
+	material.albedo_color.b8 = int(data["Color_B"])
 	
 	set_surface_override_material(0, material)
